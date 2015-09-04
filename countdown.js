4737fec70dd549fbe87f8e2ad16be5fce9d17fd4
jinweizhen <jinjin@jinjindeMacBook-Air.local>
2015-09-04 22:04:40 +0800

    数值动态化

--- countdown.js
+++ countdown.js
@@ -8,6 +8,8 @@ var RADIUS=8;
 var MARGIN_TOP=60;
 var MARGIN_LEFT=30;
 
+const endTime=new Date(2015,8,5,22,0,0);
+var ourShowTimeSeconds=0;
 
 window.onload=function(){
     var canvas=document.getElementById("canvas");
@@ -16,14 +18,22 @@ window.onload=function(){
     canvas.width=WINDOW_WIDTH;
     canvas.height=WINDOW_HEIGHT;
 
+    ourShowTimeSeconds=getCurrentShowTimeSeconds();
+
     render(context);
 }
 
+function getCurrentShowTimeSeconds(){
+    var curTime=new Date();
+    var ret=endTime.getTime()-curTime.getTime();
+    ret=Math.round(ret/1000);
+    return ret >=0?ret:0;
+}
 
 function render(cxt){
-    var hours=12;
-    var minutes=34;
-    var seconds=56;
+    var hours=parseInt(ourShowTimeSeconds/3600);
+    var minutes=parseInt((ourShowTimeSeconds-hours*3600)/60);
+    var seconds=ourShowTimeSeconds%60;
 
     renderDigit(MARGIN_LEFT,MARGIN_TOP,parseInt(hours/10),cxt);
     renderDigit(MARGIN_LEFT+15*(RADIUS+1),MARGIN_TOP,parseInt(hours%10),cxt);
